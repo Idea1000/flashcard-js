cfb10c62d2fcc65a9d960e51aef3c302051d3bae
Louis LAZARE <louislazare.pro@gmail.com>
2025-12-10 15:24:19 +0100

    [FEAT] : Add user routes & update server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRoutes from './router/authRouter.js'
+import userRoutes from "./router/userRoutes.js"
 
 const PORT = process.env.PORT || 3000
 
@@ -8,6 +9,7 @@ const app = express()
 app.use(express.json())
 
 app.use('/auth', authRoutes)
+app.use('/users', userRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}...`)
