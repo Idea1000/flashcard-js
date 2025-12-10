4d98b9b177923fe3872f0a419acfb37eb1fc1aac
Louis LAZARE <louislazare.pro@gmail.com>
2025-12-10 15:44:24 +0100

    [FIX] : Fix bug

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import express from "express"
-import authRoutes from './router/authRouter.js'
-import userRoutes from "./router/userRoutes.js"
+import authRoutes from './routers/authRouter.js'
+import userRoutes from './routers/userRouter.js'
+import flashcardRoutes from './routers/flashcardRouter.js'
+import collectionRoutes from './routers/collectionRouter.js'
+import levelRoutes from './routers/levelRouter.js'
+import revisionRoutes from './routers/revisionRouter.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -10,6 +14,10 @@ app.use(express.json())
 
 app.use('/auth', authRoutes)
 app.use('/users', userRoutes)
+app.use('/flashcard', flashcardRoutes)
+app.use('/collection', collectionRoutes)
+app.use('/level', levelRoutes)
+app.use('/revision', revisionRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}...`)
