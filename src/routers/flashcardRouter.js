6d6841c75c76a12f09c7335cad44d5aecf45838b
Louis LAZARE <louislazare.pro@gmail.com>
2025-12-11 14:05:53 +0100

    [FEAT] : Add rest route

--- src/routers/flashcardRouter.js
+++ src/routers/flashcardRouter.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
-import { createFlashcard, getFlashcardById } from "../controllers/flashcardController.js";
+import { 
+    createFlashcard, 
+    getFlashcardById, 
+    getFlashcardInCollecionById, 
+    getDueFlashcards, 
+    updateFlashcard, 
+    deleteFlashcard, 
+    reviseFlashcard 
+} from "../controllers/flashcardController.js";
 
 const router = Router()
 
 router.post('/', createFlashcard);
 router.get('/:id', getFlashcardById);
+router.get('/collection/:collectionId', getFlashcardInCollecionById)
+router.get('/collection/:collectionId/revisions/due', getDueFlashcards);
+router.put('/:id', updateFlashcard);
+router.delete('/:id', deleteFlashcard);
+router.post('/:flashcardId/revise', reviseFlashcard);
+
 
 export default router;
