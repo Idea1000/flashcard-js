ce66e73d67d7c2c7ff2cd90b59008ae243252ecf
hugotheault <hugo.theault.1@gmail.com>
2025-12-11 14:19:24 +0100

    feat: getUsers with admin restriction

---
+++ src/controllers/userController.js
@@ -0,0 +1,94 @@
+import { db } from '../db/database.js'
+import { usersTable } from '../db/schema.js'
+import { request, response } from 'express'
+import { eq } from "drizzle-orm"
+
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ */
+export const getUsers = async (req, res) => {
+    try {
+        const result = await db.select({
+            id: usersTable.id,
+            name: usersTable.name,
+            firstname: usersTable.firstname,
+            email: usersTable.email,
+            role: usersTable.role,
+            created_at: usersTable.createdAt
+        })
+            .from(usersTable)
+
+        res.status(200).json(result)
+    } catch(error){
+        console.log(error)
+        return res.status(401).send({
+            error: "You are a not allowed to see this information"
+        })
+    }
+}
+
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ */
+export const getUsersById = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const [result] = await db.select({
+            id: usersTable.id,
+            name: usersTable.name,
+            firstname: usersTable.firstname,
+            email: usersTable.email,
+            role: usersTable.role
+        })
+            .from(usersTable)
+            .where(eq(usersTable.id, id))
+
+        if (!result) {
+            res.status(404).send({
+                error: "This user doesn't exist"
+            })
+        }
+        res.status(200).json(result)
+    } catch(error){
+        console.log(error)
+        return res.status(401).send({
+            error: "You are a not allowed to see this information"
+        })
+    }
+}
+
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ */
+export const deleteUsersById = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const [result] = await db
+            .delete(usersTable)
+            .where(eq(usersTable.id, id))
+            .returning()
+        if(!result){
+            res.status(404).send({
+                error: `User ${id} not found`
+            })
+        }
+        res.status(200).send({
+            message: `user ${id} deleted successfuly`
+        })
+
+        res.status(200).json(result)
+    } catch(error){
+        console.log(error)
+        return res.status(401).send({
+            error: "You are a not allowed to see this information"
+        })
+    }
+}
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,14 @@
 import { Router } from 'express'
+import { deleteUsersById, getUsers, getUsersById } from '../controllers/userController.js'
+import { checkToken } from '../middleware/checkToken.js'
+import { checkAdmin } from '../middleware/checkAdmin.js'
 
 const router = Router()
 
-export default router;
+router.use(checkToken)
+router.use(checkAdmin)
+
+router.get('/', getUsers)
+router.get('/:id', getUsersById)
+router.delete('/:id', deleteUsersById)
+export default router
