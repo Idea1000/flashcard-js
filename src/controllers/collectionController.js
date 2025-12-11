97acdec1bbb7db898f4667f62ad39c73bdc3f235
Idea1000 <brault231@etu.unicaen.fr>
2025-12-11 14:06:21 +0100

    working collection token

--- src/controllers/collectionController.js
+++ src/controllers/collectionController.js
@@ -33,9 +33,33 @@ export const getCollectionById = async (req, res) => {
 
     try {
         const [result] = await db
-            .select()
+            .select({
+                id,
+                title,
+                description
+            })
             .from(collectionsTable)
             .where(and(eq(collectionsTable.visibility, "PUBLIC"), eq(collectionsTable.id, id)))
+
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).send({
+            error: "Failed to fetch collections",
+        })
+    }
+}
+
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ */
+export const getPersonalCollections = async (req, res) => {
+    try {
+        const result = await db
+            .select()
+            .from(collectionsTable)
+            .where(eq(collectionsTable.creatorId, req.userId.userId))
 
         res.status(200).json(result)
     } catch (error) {
@@ -59,7 +83,8 @@ export const CreateCollection = async (req, res) => {
             .values({
                 title,
                 description,
-                visibility
+                visibility,
+                creatorId: req.userId.userId
             })
 
         res.status(200).json({
@@ -73,45 +98,6 @@ export const CreateCollection = async (req, res) => {
     }
 }
 
-/**
- * 
- * @param {request} req 
- * @param {response} res 
- */
-export const DeleteCollection = async (req, res) => {
-    const { id } = req.params
-    
-    try {
-        const [result] = await db
-            .select()
-            .from(collectionsTable)
-            .where(eq(collectionsTable.id, id))
-        
-        
-        /*if (result.author !== req.userId.userId) {
-            res.status(403).send({
-                error: `You can't delete other people question`
-            })
-        }*/
-
-        const [resultDelete] = await db
-            .delete(collectionsTable)
-            .where(eq(collectionsTable.id, id))
-            .returning()
-        if(!resultDelete){
-            res.status(404).send({
-                error: `Collection ${id} not found`
-            })
-        }
-        res.status(200).send({
-            message: `Collection ${id} deleted successfuly`
-        })
-    } catch (error) {
-        res.status(500).send({
-            error: "Failed to delete collection",
-        })
-    }
-}
 
 /**
  * 
@@ -127,11 +113,11 @@ export const UpdateCollection = async (req, res) => {
             .from(collectionsTable)
             .where(eq(collectionsTable.id, id))
         
-        /*if (result.author !== req.userId.userId) {
+        if (result.creatorId !== req.userId.userId) {
             res.status(403).send({
-                error: `You can't delete other people question`
+                error: `You can't update other people collection`
             })
-        }*/
+        }
 
         let update = {}
         if (title) {
@@ -160,6 +146,46 @@ export const UpdateCollection = async (req, res) => {
     } catch (error) {
         res.status(500).send({
             error: "Failed to update collection",
+        })
+    }
+}
+
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ */
+export const DeleteCollection = async (req, res) => {
+    const { id } = req.params
+    
+    try {
+        const [result] = await db
+            .select()
+            .from(collectionsTable)
+            .where(eq(collectionsTable.id, id))
+        
+        
+        if (result.creatorId !== req.userId.userId) {
+            res.status(403).send({
+                error: `You can't delete other people question`
+            })
+        }
+
+        const [resultDelete] = await db
+            .delete(collectionsTable)
+            .where(eq(collectionsTable.id, id))
+            .returning()
+        if(!resultDelete){
+            res.status(404).send({
+                error: `Collection ${id} not found`
+            })
+        }
+        res.status(200).send({
+            message: `Collection ${id} deleted successfuly`
+        })
+    } catch (error) {
+        res.status(500).send({
+            error: "Failed to delete collection",
         })
     }
 }
--- src/routers/collectionRouter.js
+++ src/routers/collectionRouter.js
@@ -1,12 +1,19 @@
 import { Router } from 'express'
-import { getCollectionById, getPublicCollections, UpdateCollection, CreateCollection, DeleteCollection } from '../controllers/collectionController.js'
+import { checkToken } from '../middleware/checkToken.js'
+import { getCollectionById, getPublicCollections, getPersonalCollections, UpdateCollection, CreateCollection, DeleteCollection } from '../controllers/collectionController.js'
 
 const router = Router()
 
+router.use(checkToken)
+
 router.get('/public', getPublicCollections)
 router.get('/:id', getCollectionById)
+router.get('/', getPersonalCollections)
+
 router.post('/', UpdateCollection)
-router.put('/:id', CreateCollection)
+
+router.put('/', CreateCollection)
+
 router.delete('/:id', DeleteCollection)
 
 export default router
