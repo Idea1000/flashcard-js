a6149a4fd052431746731d0e1c5a47eabc52819e
Idea1000 <comistery@gmail.com>
2025-12-17 09:25:42 +0100

    better collection comment and error handling

--- src/routers/collectionRouter.js
+++ src/routers/collectionRouter.js
@@ -1,22 +1,21 @@
 import { Router } from 'express'
 import { checkToken } from '../middleware/checkToken.js'
-import { validateBody } from "../middleware/validation.js";
-import { updateCollection, createCollection } from "../models/collection.js";
-import { getCollectionById, getPublicCollections, getPersonalCollections, UpdateCollection, CreateCollection, DeleteCollection } from '../controllers/collectionController.js'
+import { validateBody } from "../middleware/validation.js"
+import { updateCollection, createCollection } from "../models/collection.js"
+import { getCollectionById, getPublicCollections, getAllPublicCollections, getPersonalCollections, UpdateCollection, CreateCollection, DeleteCollection } from '../controllers/collectionController.js'
 
 const router = Router()
 
 router.use(checkToken)
-
 router.get('/public/:name', getPublicCollections)
 // Return all public collections if no name given
-router.get('/public/', getPublicCollections)
+router.get('/public/', getAllPublicCollections)
 router.get('/:id', getCollectionById)
 router.get('/', getPersonalCollections)
 
 router.post('/', validateBody(createCollection), CreateCollection)
 
-router.put('/', validateBody(updateCollection), UpdateCollection)
+router.put('/:id', validateBody(updateCollection), UpdateCollection)
 
 router.delete('/:id', DeleteCollection)
 
