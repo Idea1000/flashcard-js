9add13e6075a5ccb064d8e398187381f677443b6
Louis LAZARE <louislazare.pro@gmail.com>
2025-12-17 07:58:15 +0100

    [FIX] : Add flashcard route
    Because of a bug I don't understand, I need to copy/paste the route manualy

--- src/routers/flashcardRouter.js
+++ src/routers/flashcardRouter.js
@@ -1,5 +1,32 @@
 import { Router } from "express";
+import { 
+    createFlashcard, 
+    getFlashcardById, 
+    getFlashcardInCollecionById, 
+    getDueFlashcards, 
+    updateFlashcard, 
+    deleteFlashcard, 
+    reviseFlashcard 
+} from "../controllers/flashcardController.js";
+import { checkToken } from "../middleware/checkToken.js";
+import { validateBody, validateParams } from "../middleware/validation.js";
+import { 
+    createFlashcardSchema, 
+    updateFlashcardSchema, 
+    flashcardIdSchema, 
+    collectionIdSchema 
+} from "../models/flashcard.js";
 
 const router = Router()
+
+router.use(checkToken)
+
+router.post('/', validateBody(createFlashcardSchema), createFlashcard);
+router.get('/:id', validateParams(flashcardIdSchema), getFlashcardById);
+router.get('/collection/:collectionId', validateParams(collectionIdSchema), getFlashcardInCollecionById)
+router.get('/collection/:collectionId/revisions/due', validateParams(collectionIdSchema), getDueFlashcards);
+router.put('/:id', validateParams(flashcardIdSchema), validateBody(updateFlashcardSchema), updateFlashcard);
+router.delete('/:id', validateParams(flashcardIdSchema), deleteFlashcard);
+router.post('/:flashcardId/revise', validateParams(flashcardIdSchema), reviseFlashcard);
 
 export default router;
